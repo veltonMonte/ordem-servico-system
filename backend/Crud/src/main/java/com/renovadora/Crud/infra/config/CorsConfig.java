03c2c230cda9a2feee62789dc45d99516a6a9c70
veltonMonte <montevelton@gmail.com>
2026-02-17 22:27:43 -0300

    Fix CORS configuration for allowed origins

--- backend/Crud/src/main/java/com/renovadora/Crud/infra/config/CorsConfig.java
+++ backend/Crud/src/main/java/com/renovadora/Crud/infra/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(""https://ordem-servico-system.vercel.app"")); // seu front
+        config.setAllowedOrigins(List.of(""https://ordem-servico-system.vercel.app""));
         config.setAllowCredentials(true);
         config.addAllowedHeader("*");
         config.addAllowedMethod("*"); // GET, POST, PUT, DELETE, OPTIONS
