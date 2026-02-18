f385aba1de1aa5cb8d0290724966f1c18102f6f8
veltonMonte <montevelton@gmail.com>
2026-02-17 21:56:15 -0300

    Change API base URL to production server

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://ordem-servico-system.onrender.com
+",
   withCredentials: true,
 });
 
